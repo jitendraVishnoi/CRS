f314c6e9f1dede146e902b52fcc4f4f352b5d7e3
jitubaba10 <vishnoi.jitendra@gmail.com>
2015-09-08 22:43:08 +0530

    integration changes

--- EStore/src/crs/projects/store/order/purchase/CRSBillingInfoFormHandler.java
+++ EStore/src/crs/projects/store/order/purchase/CRSBillingInfoFormHandler.java
@@ -2,82 +2,78 @@ package crs.projects.store.order.purchase;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.servlet.ServletException;
 
-import crs.paypal.ExternalPaymentManager;
-
 import atg.commerce.CommerceException;
-import atg.commerce.order.OrderHolder;
+import atg.commerce.order.HardgoodShippingGroup;
+import atg.commerce.order.Order;
+import atg.commerce.order.ShippingGroup;
+import atg.commerce.order.purchase.ShippingGroupContainerService;
 import atg.core.util.StringUtils;
-import atg.droplet.DropletException;
 import atg.projects.store.order.purchase.BillingInfoFormHandler;
+import atg.repository.RepositoryItem;
 import atg.servlet.DynamoHttpServletRequest;
 import atg.servlet.DynamoHttpServletResponse;
-
+import crs.paypal.PaypalManager;
 
 public class CRSBillingInfoFormHandler extends BillingInfoFormHandler{
 	
 	private String paymentType;
-	private ExternalPaymentManager paymentManager;
-	private OrderHolder shoppingCart;
-	private String returnUrl, cancelUrl;
+	private PaypalManager paypalManager;
+	private String payPalReturnUrl, payPalCancelUrl;
+	private ShippingGroupContainerService shippingGroupContainerService;
 	
 	@Override
 	public boolean handleBillingWithNewAddressAndNewCard(
 			DynamoHttpServletRequest pRequest,
 			DynamoHttpServletResponse pResponse) throws ServletException,
 			IOException, CommerceException {
-
+		
 		if (StringUtils.isBlank(getPaymentType())) {
 			return super.handleBillingWithNewAddressAndNewCard(pRequest, pResponse);
 		} else {
-			updateBillingAddress();
-			return initiatePayment(pRequest, pResponse);
-		}
-	}
-
-	private boolean initiatePayment(DynamoHttpServletRequest pRequest,
-			DynamoHttpServletResponse pResponse) throws ServletException,
-			IOException {
-		
-		
-		HashMap nvp = getPaymentManager().callMarkExpressCheckout(
-				String.valueOf(getShoppingCart().getCurrent().getPriceInfo().getAmount()), 
-				getReturnUrl(), getCancelUrl());
-		String strAck = nvp.get("ACK").toString();
-		if(strAck !=null && (strAck.equalsIgnoreCase("Success") || strAck.equalsIgnoreCase("SuccessWithWarning")))
-		{
-			//session.setAttribute("token", nvp.get("TOKEN").toString());
-			//' Redirect to paypal.com
-			String payPalURL = "https://www.sandbox.paypal.com/webscr?cmd=_express-checkout&token=" + nvp.get("TOKEN");
-
-		    //response.sendRedirect( payPalURL );
-			pResponse.setStatus(302);
-			pResponse.setHeader( "Location", payPalURL );
-			pResponse.setHeader( "Connection", "close" );
-
+			addNewAddress();
+			initiatePayment();
 			return true;
 		}
-		else
-		{
-			// Display a user friendly Error on the page using any of the following error information returned by PayPal
-
-			String ErrorCode = nvp.get("L_ERRORCODE0").toString();
-			String ErrorShortMsg = nvp.get("L_SHORTMESSAGE0").toString();
-			String ErrorLongMsg = nvp.get("L_LONGMESSAGE0").toString();
-			String ErrorSeverityCode = nvp.get("L_SEVERITYCODE0").toString();
-			vlogError("error while payment with paypal {0}", ErrorLongMsg);
-			addFormException(new DropletException(ErrorLongMsg));
-			return false;
-		}
-			
 	}
+
 	
-	private void updateBillingAddress() {
+	private void initiatePayment() {
+		HashMap<String, String> details = new HashMap<String, String>();
+		details.put("returnUrl", getPayPalReturnUrl());
+		details.put("cancelUrl", getPayPalCancelUrl());
+		Map<String, ShippingGroup> shippingGroups = getShippingGroupContainerService().getShippingGroupMap();
+		HardgoodShippingGroup defaultAddress  = null;
+		for (Entry<String, ShippingGroup> entry : shippingGroups.entrySet()) {
+			if (entry.getKey().equals(getStoredAddressSelection())) {
+				defaultAddress = (HardgoodShippingGroup) entry.getValue();
+				break;
+			}
+		}
+		details.put("returnUrl", getPayPalReturnUrl());
+		details.put("cancelUrl", getPayPalReturnUrl());
+		details.put("shipToName", defaultAddress.getShippingAddress().getFirstName()); 
+		details.put("shipToStreet", defaultAddress.getShippingAddress().getAddress1());
+		details.put("shipToCity",defaultAddress.getShippingAddress().getCity()); 
+		details.put("shipToState", defaultAddress.getShippingAddress().getState());
+		details.put("shipToCountryCode", defaultAddress.getShippingAddress().getCountry()); 
+		details.put("shipToZip", defaultAddress.getShippingAddress().getPostalCode());
+		details.put("shipToStreet2", defaultAddress.getShippingAddress().getAddress2());
+		//details.put("phoneNum"), defaultAddress.get);
+		
+		getPaypalManager().callExpressCheckout(getOrder(), details);
+	}
+
+
+	private void addNewAddress() {
 		// TODO Auto-generated method stub
 		
 	}
+
 
 	@Override
 	public boolean handleBillingWithSavedAddressAndNewCard(
@@ -87,50 +83,61 @@ public class CRSBillingInfoFormHandler extends BillingInfoFormHandler{
 		if (StringUtils.isBlank(getPaymentType())) {
 			return super.handleBillingWithSavedAddressAndNewCard(pRequest, pResponse);
 		} else {
-			return initiatePayment(pRequest, pResponse);
+			initiatePayment();
+			return true;
 		}
-	}
-	
-	
-
-	public ExternalPaymentManager getPaymentManager() {
-		return paymentManager;
+		
 	}
 
-	public void setPaymentManager(ExternalPaymentManager paymentManager) {
-		this.paymentManager = paymentManager;
-	}
 
 	public String getPaymentType() {
 		return paymentType;
 	}
 
+
 	public void setPaymentType(String paymentType) {
 		this.paymentType = paymentType;
 	}
 
-	public OrderHolder getShoppingCart() {
-		return shoppingCart;
+
+	public PaypalManager getPaypalManager() {
+		return paypalManager;
 	}
 
-	public void setShoppingCart(OrderHolder shoppingCart) {
-		this.shoppingCart = shoppingCart;
+
+	public void setPaypalManager(PaypalManager paypalManager) {
+		this.paypalManager = paypalManager;
 	}
 
-	public String getReturnUrl() {
-		return returnUrl;
+
+	public String getPayPalReturnUrl() {
+		return payPalReturnUrl;
 	}
 
-	public void setReturnUrl(String returnUrl) {
-		this.returnUrl = returnUrl;
+
+	public void setPayPalReturnUrl(String payPalReturnUrl) {
+		this.payPalReturnUrl = payPalReturnUrl;
 	}
 
-	public String getCancelUrl() {
-		return cancelUrl;
+
+	public String getPayPalCancelUrl() {
+		return payPalCancelUrl;
 	}
 
-	public void setCancelUrl(String cancelUrl) {
-		this.cancelUrl = cancelUrl;
+
+	public void setPayPalCancelUrl(String payPalCancelUrl) {
+		this.payPalCancelUrl = payPalCancelUrl;
 	}
-	
+
+
+	public ShippingGroupContainerService getShippingGroupContainerService() {
+		return shippingGroupContainerService;
+	}
+
+
+	public void setShippingGroupContainerService(
+			ShippingGroupContainerService shippingGroupContainerService) {
+		this.shippingGroupContainerService = shippingGroupContainerService;
+	}
+
 }
